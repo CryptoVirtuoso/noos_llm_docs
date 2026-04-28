b05f3755c18a1cdc5518c9dc03e73644ddb4cb87
thstoqd <92540911+CryptoVirtuoso@users.noreply.github.com>
2026-04-28 18:34:17 +0800

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,12 +4,12 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>Noos LLM Docs</span>,
   project: {
-    link: 'https://github.com/CryptoVirtuoso/noos_llm_docs,
+    link: 'https://github.com/',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/CryptoVirtuoso/noos_llm_docs',
+  docsRepositoryBase: 'https://github.com/',
   footer: {
     text: 'Nextra Docs Template',
   },
